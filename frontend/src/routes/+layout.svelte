1f7f3c75887c38306a8b1ebe2a53e62cfcc78cb6
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2025-09-20 20:11:36 -0700

    added reactive local data storing

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,11 +2,54 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { onMount } from 'svelte';
-
+  	import io from 'socket.io-client';
+	
 	let { children } = $props();
 
+	// Client's "full" local database. NOT sent to the server, only received.
+	let localRoomData = $state({
+			id: 'cascadia',
+			name: 'Cascadia Hackathon',
+			users: []
+	});
+
+	// And this is the client's OWN data, that it will SEND to the server.
+	let userData = $state({
+		name: "SAMPLE USERNAME",
+		// No userId - will be assigned by Supabase
+		linkedin_url: "https://www.linkedin.com/in/sample-username/",
+		bio: "SDE @ Hewlett Packard Enterprise",
+		headshot_image: "https://media.licdn.com/dms/image/D4D03AQHjv1eXHk1m7g/profile-displayphoto-shrink_800_800/0/1678887038471?e=1701302400&v=beta&t=ZKJ3nY3c1kC8qfO2nF9lJd8nG4p6Ykz8b9rY3F4g6Uo",
+		location: {
+			latitude: 47.6062,
+			longitude: -122.3321
+		}
+	});
+
+	// Initiate socket connections on mount
 	onMount(() => {
-		console.log('layout mounted');
+		console.log('Layout mounted.');
+
+		// Link to the Express app server running socketio
+		const socket = io('https://6f3ad484c5c1.ngrok-free.app');
+
+		// Join the server room 
+		function joinRoom(userData) {
+			socket.emit("join-room", userData);
+		}
+		joinRoom(userData);
+
+		// This is how to listen for requests to get this user's location
+		socket.on('get-location', () => {
+			socket.emit('send-location', userData)
+		})
+
+		// This is how to listen for the server sending all user locations
+		socket.on("update-data", (data) => {
+			localRoomData = data;
+			console.log(JSON.stringify(localRoomData));
+		});
+
 	});
 </script>
 
